110b750dd100574773957d261d02415f730d35e3
Chris Meyer <cmeyer8998@gmail.com>
2016-06-12 14:06:57 -0500

    Reduced multiplication factor to fix scrolling

--- scripts/cpscripts.js
+++ scripts/cpscripts.js
@@ -1,20 +1,24 @@
 jQuery(document).ready(function ($) {
+//Determines how far to "look ahead" when considering the vertical space
+ var heightFactor = 0.2;
   console.log('window.scrollTop() is '+$(window).scrollTop());
   console.log('window.height is '+$(window).height());
-  console.log('math result is '+ ($(window).scrollTop() + $(window).height() * 0.75));
+  //console.log('init vertical range is '+ ($(window).scrollTop() + $(window).height() * 0.75));
+  console.log('init vertical range is '+ ($(window).scrollTop() + $(window).height() * heightFactor));
     var $timeline_block = $('.cd-timeline-block');
     $timeline_block.each(function (i,v) {
         console.log('offset().top for '+i+' is '+$(this).offset().top);
 
-        if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
+        if ($(this).offset().top > $(window).scrollTop() + $(window).height() * heightFactor) {
             console.log("hiding "+i);
             $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
         }
     });
     $(window).on('scroll', function () {
+        console.log("new vertical range: " + ($(window).scrollTop() + $(window).height() * heightFactor));
         $timeline_block.each(function (i,v) {
-            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
-              console.log("onscroll: showing "+i);
+            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * heightFactor && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
+              console.log("onscroll: showing "+i+" because top offset within range");
                 $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
             }
         });
